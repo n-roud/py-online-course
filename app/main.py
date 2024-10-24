613c72ac54809bd9eebdeead1581569f017500ee
Anastasiia Khudyakova <sun14water16@gmail.com>
2024-10-24 19:51:08 +0300

    3st push

--- app/main.py
+++ app/main.py
@@ -11,12 +11,12 @@ class OnlineCourse:
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
-        return math.ceil(days/7)
+        return math.ceil(days / 7)
 
     @classmethod
-    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+    def from_dict(cls, course_dict: dict) -> OnlineCourse | None:
         if "days" in course_dict:
             weeks = cls.days_to_weeks(course_dict["days"])
             return cls(course_dict["name"], course_dict["description"], weeks)
         else:
-            raise ValueError("Ключ 'days' не знайдено в словнику")
+            return print("Сталася помилка: ValueError")
